a115de4dde74e1a2d752ba62ef7ea67624b20ad7
Kushwaharahul025 <rahuldatatuploadrahul@gmail.com>
2025-09-19 23:17:43 +0530

    Update variable.tf

--- dev/variable.tf
+++ dev/variable.tf
@@ -1,11 +1,3 @@
-# variable "rg_name" {}
-# variable "location" {}
-variable "rg1" {}
-variable "vnet" {}
-variable "sub1" {}
-variable "nsg1" {}
-variable "nic" {}
-variable "vm1" {}
-variable "pip" {}
+
 
 
